953dcfa000ae49f8c24bf456cda3b4f7335e9b71
Gianna Nacuzzi <giannanacuzzi@MBP-de-Gianna.fibertel.com.ar>
2024-11-16 21:16:52 -0300

    tablet : ingresar y cobrar

--- app/src/main/java/com/example/peraapp/tablet/DepositTablet.kt
+++
@@ -1,4 +0,0 @@
-package com.example.peraapp.tablet
-
-class DepositTablet {
-}
--- app/src/main/java/com/example/peraapp/tablet/HomeTablet.kt
+++ app/src/main/java/com/example/peraapp/tablet/HomeTablet.kt
@@ -2,6 +2,7 @@ package com.example.peraapp.tablet
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
@@ -19,8 +20,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -28,6 +31,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -41,13 +45,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.example.peraapp.CardHome
+import com.example.peraapp.Deposit
 import com.example.peraapp.MovimientoItem
 import com.example.peraapp.R
 import com.example.peraapp.ui.theme.PeraAppTheme
+import kotlinx.coroutines.handleCoroutineException
 
 class HomeTablet {
 }
@@ -373,6 +381,219 @@ fun MovimientosSectionTablet() {
                     color = Color.Red,
                     arrangement = Arrangement.SpaceBetween
                 )
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ChargeDialogPreview() {
+    PeraAppTheme {
+        ChargeDialog(
+            onDismissRequest = { },
+            onConfirmation = { },
+            dialogTitle = "Cobrar"
+            //habria que pasar el monto
+        )
+    }
+}
+
+@Composable
+fun ChargeDialog(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String
+) {
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        Surface(
+            shape = RoundedCornerShape(12.dp),
+            color = MaterialTheme.colorScheme.surface,
+            tonalElevation = 8.dp,
+            modifier = Modifier.width(800.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row (
+                    modifier = Modifier.fillMaxWidth()
+                ){
+                    IconButton(
+                        onClick = { onDismissRequest() },
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Volver atrás",
+                            tint = MaterialTheme.colorScheme.onBackground,
+                            modifier = Modifier.size(100.dp)
+                        )
+                    }
+                    Text(
+                        text = dialogTitle,
+                        style = MaterialTheme.typography.displayMedium,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.padding(bottom = 16.dp).align(Alignment.CenterVertically)
+                    )
+                }
+                Text(
+                    text = "Ingresa el monto:",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = { /* Manejar el cambio de valor */ },
+                    label = { Text("Monto") },
+                    modifier = Modifier
+                        .padding(bottom = 10.dp, top = 20.dp)
+                        .align(Alignment.CenterHorizontally),
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number
+                    ),
+                    textStyle = MaterialTheme.typography.titleLarge
+                )
+
+                Button(
+                    onClick = { /* Acción para transferir */ },
+                    modifier = Modifier
+                        .padding(top = 20.dp)
+                        .width(270.dp)
+                        .align(Alignment.CenterHorizontally),
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = MaterialTheme.colorScheme.secondary,
+                        containerColor = MaterialTheme.colorScheme.primary
+                    )
+                ) {
+                    Text("Generar link", style = MaterialTheme.typography.titleLarge)
+                }
+
+                Text(
+                    text = "Link generado: https:/aparece..cuando..apretas",
+                    modifier = Modifier.padding(top = 20.dp),
+                    style = MaterialTheme.typography.titleMedium
+                )
+
+                Button(//deberia solo aparecer una vez que apretas generar link
+                    onClick = { onConfirmation() },
+                    modifier = Modifier
+                        .padding(top = 20.dp)
+                        .width(270.dp)
+                        .align(Alignment.CenterHorizontally),
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = MaterialTheme.colorScheme.secondary,
+                        containerColor = MaterialTheme.colorScheme.primary
+                    )
+                ) {
+                    Text("Compartir", style = MaterialTheme.typography.titleLarge)
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DepositDialogPreview() {
+    PeraAppTheme {
+        DepositDialog(
+            onDismissRequest = { },
+            onConfirmation = { },
+            dialogTitle = "Ingresar"
+            //habria que pasar el monto
+        )
+    }
+}
+
+@Composable
+fun DepositDialog(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String
+) {
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        Surface(
+            shape = RoundedCornerShape(12.dp),
+            color = MaterialTheme.colorScheme.surface,
+            tonalElevation = 8.dp,
+            modifier = Modifier.width(800.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    IconButton(
+                        onClick = { onDismissRequest() },
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Volver atrás",
+                            tint = MaterialTheme.colorScheme.onBackground,
+                            modifier = Modifier.size(100.dp)
+                        )
+                    }
+                    Text(
+                        text = dialogTitle,
+                        style = MaterialTheme.typography.displayMedium,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.padding(bottom = 16.dp)
+                            .align(Alignment.CenterVertically)
+                    )
+                }
+                Text(
+                    text = "Tu CBU",
+                    modifier = Modifier.padding(start = 10.dp, bottom = 20.dp),
+                    style = MaterialTheme.typography.titleLarge
+                )
+
+                Text(
+                    text = "00000121213242434354545", //hacerlo responsivo
+                    modifier = Modifier
+                        .padding(bottom = 20.dp)
+                        .border(1.dp, MaterialTheme.colorScheme.tertiary)
+                        .padding(8.dp),
+                    style = MaterialTheme.typography.titleLarge
+                )
+
+                Text(
+                    text = "Tu Alias",
+                    modifier = Modifier.padding(start = 10.dp, bottom = 20.dp),
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Text(
+                    text = "pera.app", //hacerlo responsivo
+                    modifier = Modifier
+                        .border(1.dp, MaterialTheme.colorScheme.tertiary)
+                        .padding(8.dp),
+                    style = MaterialTheme.typography.titleLarge
+                )
+
+                Button(//deberia solo aparecer una vez que apretas generar link
+                    onClick = { onConfirmation() },
+                    modifier = Modifier
+                        .padding(top = 60.dp)
+                        .width(270.dp)
+                        .align(Alignment.CenterHorizontally),
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = MaterialTheme.colorScheme.secondary,
+                        containerColor = MaterialTheme.colorScheme.primary
+                    )
+                ) {
+                    Text("Compartir datos", style = MaterialTheme.typography.titleMedium)
+                }
             }
         }
     }
